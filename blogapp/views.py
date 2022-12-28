ee57e9f59c07d1953fe0c8cb6c98670f2e8259f6
attoh alexander <alexander.attoh22@gmail.com>
2022-12-28 14:24:21 +0100

    create and populate Post model

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,36 +1,19 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = (
-    {
-        'author': 'Attoh-Alexander',
-        'title': 'Blog post 1',
-        'content': 'first post content',
-        'date_posted': '25th december 2022'
-    },
-    {
-        'author': 'Attoh-Tobe',
-        'title': 'Blog post 2',
-        'content': 'second post content',
-        'date_posted': '27th december 2022'
-    },
-    {
-        'author': 'Attoh-Chico',
-        'title': 'Blog post 3',
-        'content': 'third post content',
-        'date_posted': '30th december 2022'
-    }
-)
 
 # Create your views here.
 
-
 def home(request):
     # django database
-    context = {'posts': posts}
+    context = {'posts': Post.objects.all()}
 
     # render html template
     return render(request, 'blogapp/home.html', context)
 
 
 def about(request):
+    # django database
+
+    # render html template
     return render(request, 'blogapp/about.html', {'title': 'About'})
